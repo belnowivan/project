b59bc9c59c28155c43110dacd76c09065a00c29e
ivan <morphizm@ya.ru>
2019-06-10 22:44:40 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const brainGame = () => {
   console.log('Welcome to the Brain Games!');
-  const yourName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${yourName} !`);
+  const nameOfUser = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${nameOfUser} !`);
 };
 export default brainGame;
